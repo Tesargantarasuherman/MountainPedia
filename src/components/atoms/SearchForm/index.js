1560918f0f98c68f48d18b3e388d1c3d9b6008e5
unknown <tgantara@gmail.com>
2022-11-01 19:57:43 +0700

    update

--- src/components/atoms/SearchForm/index.js
+++ src/components/atoms/SearchForm/index.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { Location } from '../../../assets'
 import './index.scss'
 
-function InputForm({label,icon,gap}) {
+function InputForm({ label, icon, gap }) {
   return (
     <>
       <div className="search-form-input">
-        <label>
-          <img src={icon} /> {label}
-        </label>
-        <select name="" id="">
-          <option value="">lorem</option>
-        </select>
+        <img src={icon} />
+        <div>
+          <label>
+            {label}
+          </label>
+          {/* <select name="" id="">
+            <option value="">choose {label}</option>
+          </select> */}
+          <input type="date" name="" id="" />
+        </div>
       </div>
       {
-        gap ? <hr/> : null
+        gap ? <hr /> : null
       }
     </>
   )
@@ -22,11 +26,10 @@ function InputForm({label,icon,gap}) {
 export default function SearchForm() {
   return (
     <div className='search-form'>
-      <InputForm inputForm label ="Type Trip" icon={Location} gap={true}/>
-      <InputForm inputForm label ="Type Trip" icon={Location} gap={true}/>
-      <InputForm inputForm label ="Type Trip" icon={Location} gap={true}/>
-      <InputForm inputForm label ="Type Trip" icon={Location} gap={true}/>
-      <InputForm inputForm label ="Type Trip" icon={Location} gap={false}/>
+      <InputForm inputForm label="Type Trip" icon={Location} gap={true} />
+      <InputForm inputForm label="Location" icon={Location} gap={true} />
+      <InputForm inputForm label="Date" icon={Location} gap={true} />
+      <InputForm inputForm label="Person" icon={Location} gap={true} />
       <button>Search</button>
     </div>
   )
