490f6a5995f2d5414ed3f67af3355eea44f2dffd
Tesar Gantara Suherman <tgantara@gmail.com>
2023-04-12 22:23:08 +0700

    update

--- src/components/molecules/Banner/index.js
+++ src/components/molecules/Banner/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Image, Shimmer } from 'react-shimmer'
 import { SearchForm } from '../../atoms'
 import './index.scss'
 
@@ -12,12 +13,12 @@ export default function Banner() {
           </div>
         </div>
         <div className="banner-right">
-            <img src="https://cdn.dribbble.com/users/3251575/screenshots/18004988/media/d6a01e671362052896625a63b7a960bc.jpeg?compress=1&resize=1000x750&vertical=top" alt="" srcSet="" />
+            <Image src="https://cdn.dribbble.com/users/3251575/screenshots/18004988/media/d6a01e671362052896625a63b7a960bc.jpeg?compress=1&resize=1000x750&vertical=top" alt="" srcSet="" fallback={<Shimmer width={'100%'} height={'100%'} />} />
         </div>
         <div className="banner-img-content">
-            <img src="https://images.unsplash.com/photo-1489657780376-e0d8630c4bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxhbmRzY2FwZSUyMGlsdXN0cmF0aW9ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-            <img src="https://images.unsplash.com/photo-1489657780376-e0d8630c4bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxhbmRzY2FwZSUyMGlsdXN0cmF0aW9ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
-            <img src="https://images.unsplash.com/photo-1489657780376-e0d8630c4bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxhbmRzY2FwZSUyMGlsdXN0cmF0aW9ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+            <Image src="https://images.unsplash.com/photo-1489657780376-e0d8630c4bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxhbmRzY2FwZSUyMGlsdXN0cmF0aW9ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60" fallback={<Shimmer width={50} height={50} />} />
+            <Image src="https://images.unsplash.com/photo-1489657780376-e0d8630c4bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxhbmRzY2FwZSUyMGlsdXN0cmF0aW9ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60" fallback={<Shimmer width={50} height={50} />} />
+            <Image src="https://images.unsplash.com/photo-1489657780376-e0d8630c4bd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxhbmRzY2FwZSUyMGlsdXN0cmF0aW9ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60" fallback={<Shimmer width={50} height={50} />} />
         </div>
         <SearchForm />
     </div>
--- src/components/molecules/ImageCollage/index.js
+++ src/components/molecules/ImageCollage/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Button } from '../../atoms'
 import './index.scss'
-function ImageCollage({popup}) {
+function ImageCollage({ popup }) {
   return (
     <div className='image-collage'>
-      <div className="button-view-all">
-        <Button title={'View All'} onClick={popup}/>
-      </div>
-      <div className='content-collage'>
-        <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
-        <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
-        <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
-        <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
+      <div className='desktop'>
+        <div className="button-view-all">
+          <Button title={'View All'} onClick={popup} />
+        </div>
+        <div className='content-collage'>
+          <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
+          <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
+          <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
+          <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
+        </div>
       </div>
     </div>
   )
