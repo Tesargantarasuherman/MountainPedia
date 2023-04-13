2dfc1c03a149326f9c7f0aba5341d47bc586a4f0
Tesar Gantara Suherman <tgantara@gmail.com>
2023-04-14 00:49:32 +0700

    update

--- src/components/molecules/ImageCollage/index.js
+++ src/components/molecules/ImageCollage/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Slider from "react-slick";
+import Flickity from 'react-flickity-component'
+
 import { Button } from '../../atoms'
 import './index.scss'
 function ImageCollage({ popup }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  const flickityOptions = {
+    initialIndex: 2
+  }
   return (
     <div className='image-collage'>
       <div className='desktop'>
@@ -14,6 +27,19 @@ function ImageCollage({ popup }) {
           <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
           <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" />
         </div>
+      </div>
+      <div className='mobile'>
+        <Slider {...settings}>
+          <div className='body-img-carousel-mobile'>
+            <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" className='img-carousel-mobile' />
+          </div>
+          <div className='body-img-carousel-mobile'>
+            <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" className='img-carousel-mobile' />
+          </div>
+          <div className='body-img-carousel-mobile'>
+            <img src="https://images.unsplash.com/photo-1592364395653-83e648b20cc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" srcSet="" className='img-carousel-mobile' />
+          </div>
+        </Slider >
       </div>
     </div>
   )
