0e775c0a94856d9ab83674b08f2548a0387d9939
Tesar Gantara Suherman <tgantara@gmail.com>
2023-05-09 21:29:14 +0700

    update banner

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -1,56 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import ConfigContext from '../../../context/ConfigContext'
 import { toaster } from '../../../utils/toaster'
 import './index.scss'
+import { AiOutlineUser } from 'react-icons/ai'
 
-const {AuthContext} = ConfigContext
+const { AuthContext } = ConfigContext
 export default function Navbar() {
+  const [mouseEnter,setMouseEnter] =useState(false);
   const navigate = useNavigate()
 
   const props_auth = useContext(AuthContext);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     props_auth.validationToken()
     renderButton()
-  },[])
-  
-  const logout =()=>{
+  }, [])
+
+  const logout = () => {
     localStorage.removeItem('token')
     props_auth.validationToken()
     renderButton()
     navigate('/');
-    toaster('success','Successfully Logout!')         
+    toaster('success', 'Successfully Logout!')
 
   }
-  const renderButton =()=>{
+  const actionOnMouseEnter =()=>{
+    setMouseEnter(!mouseEnter);
+  }
+  const renderButton = () => {
     if (props_auth.auth == true) {
       return (
-        <button type="button" className='btn-login' onClick={logout}>Logout</button>
+        <div className='parent-user'>
+          <AiOutlineUser onClick={logout}/>
+          {/* <button type="button" className='btn-login' >Logout</button> */}
+          <div className={`user ${mouseEnter ? 'visible': ''}`}>
+            <p>Profile</p>
+            <p>Favorite</p>
+            <p onClick={logout}>logout</p>
+          </div>
+        </div>
       )
     }
-    else{
+    else {
       return <Link className='btn-login' to='/sign'>Sign In / Register</Link>
     }
   }
 
   return (
     <div className='navbar'>
-        <div className="navbar-left">
+      <div className="navbar-left">
         <p><Link to='/'>MountainPedia</Link></p>
-        </div>
-        <div className="navbar-right">
-            <ul>
-                <li>lorem</li>
-                <li>lorem</li>
-                <li>lorem</li>
-                <li>
-                  {renderButton()}
-                    {/* <Link className='btn-login' to='/sign'>Sign In / Register</Link> */}
-                </li>
-            </ul>
-        </div>
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li>lorem</li>
+          <li>lorem</li>
+          <li>lorem</li>
+          <li>
+            {renderButton()}
+            {/* <Link className='btn-login' to='/sign'>Sign In / Register</Link> */}
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
