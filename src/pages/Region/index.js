1c72b5a527ec8f9e84f83c65ecd9636fee2d2996
tgantara@gmail.com <tgantara@gmail.com>
2023-06-05 21:59:40 +0700

    add card guide

--- src/pages/Region/index.js
+++ src/pages/Region/index.js
@@ -4,11 +4,14 @@ import { AiOutlineClose, AiOutlineEnvironment, AiOutlineSearch } from 'react-ico
 import { CardGuide, Footer, ImageCollage, MapLeaflet } from '../../components'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { iconPerson } from '../../utils/icon';
+import { MdVerified } from 'react-icons/md';
+import {IoLocationSharp} from "react-icons/io5";
+
 export default function Region() {
     const [inputForm, setInputForm] = useState('');
     return (
 
-        <section className='region' >
+        <section className='region' onClick={() => console.log('run')}>
             <div className="banner-region">
                 <div className="dimmer" />
                 <div className="body-banner" style={{ backgroundImage: `url(https://images.unsplash.com/photo-1588668214407-6ea9a6d8c272?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2342&q=80)` }}>
@@ -41,7 +44,41 @@ export default function Region() {
                     <CardGuide />
                 </div>
             </div>
-                <Footer />
+            <Footer />
+            <div className="modal-guide">
+                <div className="modal-guide-content">
+                    <div className="__icon">
+                        <AiOutlineClose />
+                    </div>
+                    <div className="info-guide">
+                        <div className="detail-name">
+                            <div className="__img-guide" style={{ backgroundImage: `url(https://a0.muscache.com/im/pictures/user/08586a25-5872-4b47-97b4-5d74678b9877.jpg?im_w=240)` }}>
+                                <MdVerified />
+                            </div>
+                            <div className="guide-name">
+                                <p>Gantara</p>
+                                <p><IoLocationSharp/> Indonesia</p>
+                            </div>
+                        </div>
+                        <div className="rating-guide">
+                            <div className='content-rating'>
+                                <p>171</p>
+                                <p>ulasan</p>
+                            </div>
+                            <hr />
+                            <div className='content-rating'>
+                                <p>171</p>
+                                <p>ulasan</p>
+                            </div>
+                            <hr />
+                            <div className='content-rating'>
+                                <p>171</p>
+                                <p>ulasan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
