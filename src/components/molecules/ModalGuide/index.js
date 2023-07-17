c1c2a63ed44699f81ce2dfc3ffd1da5035ba6a29
tgantara@gmail.com <tgantara@gmail.com>
2023-07-17 14:50:38 +0700

    validasi

--- src/components/molecules/ModalGuide/index.js
+++ src/components/molecules/ModalGuide/index.js
@@ -23,18 +23,17 @@ function ModalGuide({ active, setActive }) {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                     infinite: true,
-                    dots: true
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1
                 }
             },
             {
